57df2dc987edaf2303853a4524c8b7ff54673c53
Manjunath-Dev <manjunathpv.dev@gmail.com>
2016-09-07 21:48:17 +0530

    Revert "1"
    This reverts commit 70863757a13e97d238eb12865fb7ac3caf400923.

--- javascript/jquery.bxslider.js
+++ javascript/jquery.bxslider.js
@@ -2,7 +2,7 @@
  * BxSlider v4.1.2 - Fully loaded, responsive content slider
  * http://bxslider.com
  *
- * Copyright <?php echo Date('Y'); ?>, Steven Wanderski - http://stevenwanderski.com - http://bxcreative.com
+ * Copyright 2014, Steven Wanderski - http://stevenwanderski.com - http://bxcreative.com
  * Written while drinking Belgian ales and listening to jazz
  *
  * Released under the MIT license - http://opensource.org/licenses/MIT
